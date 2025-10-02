dd11b968c2bd674e7c2712660c4ffcc61c2ab3dd
ChristianCHJH <christianjara011215@gmail.com>
2025-10-02 13:18:30 -0500

    chore: configuración de alias de rutas y configuración enrutador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ReservasComponent } from './features/reservas/components/reservas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'reservas', pathMatch: 'full' },
+  { path: 'reservas', component: ReservasComponent }
+];
